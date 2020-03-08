79b46b9b9a68d2742d675b152954cc7833a3fbf5
sean <42746972+seanbff@users.noreply.github.com>
2020-03-08 08:26:53 -0400

    cv

---
+++ Challenge 2/Challenge 2/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        this.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+
+    }
+}
---
+++ Challenge 2/Challenge 2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    private Rigidbody2D rd2d;
+    public float speed;
+    public Text score;
+    public Text winText;
+    public Text livesText;
+    private int scoreValue = 0;
+    private int livesValue = 3;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        score.text = "Score: " + scoreValue.ToString();
+        scoreValue = 0;
+        winText.text = "";
+        livesText.text = "Lives: " + livesValue.ToString();
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+
+        float hozMovement = Input.GetAxis("Horizontal");
+        float verMovement = Input.GetAxis("Vertical");
+
+        rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = "Score: " + scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector2(30.0f, 0f);
+                livesValue = 3;
+                livesText.text = "Lives " + livesValue.ToString();
+            }
+            if (scoreValue >= 8)
+            {
+                winText.text = "You win! Game created by Sean Bergeron.";
+                musicSource.clip = musicClipTwo;
+                musicSource.Play();
+            }
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue = livesValue - 1;
+            livesText.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if (livesValue <= 0)
+            {
+                winText.text = "You lose! Game created by Sean Bergeron.";
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
